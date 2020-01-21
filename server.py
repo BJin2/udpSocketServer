2c7ad1fdf80d31fe33924dcbb442152144027d06
EC2 Default User <ec2-user@ip-172-31-82-181.ec2.internal>
2020-01-21 22:50:29 +0000

    Implemented sending client list to new client

--- server.py
+++ server.py
@@ -9,24 +9,35 @@ import json
 clients_lock = threading.Lock()
 connected = 0
 
+#indexed by ip and port
 clients = {}
 
 def connectionLoop(sock):
    while True:
       data, addr = sock.recvfrom(1024)
       data = str(data)
+      #if the address from socket is already in the client list
+      #iwhich means the client is alreday connected
       if addr in clients:
          if 'heartbeat' in data:
             clients[addr]['lastBeat'] = datetime.now()
       else:
          if 'connect' in data:
+            #If the request from the client is connect
+            print(" ")
+            print("New client connected")
+            print(" ")
             clients[addr] = {}
             clients[addr]['lastBeat'] = datetime.now()
             clients[addr]['color'] = 0
             message = {"cmd": 0,"player":{"id":str(addr)}}
+            connectedClients = {"cmd": 2, "current_players": []}
             m = json.dumps(message)
             for c in clients:
                sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
+               connectedClients['current_players'].append({"id":str(c)})
+            m = json.dumps(connectedClients)
+            sock.sendto(bytes(m,'utf8'), (addr[0],addr[1]))
 
 def cleanClients():
    while True:
