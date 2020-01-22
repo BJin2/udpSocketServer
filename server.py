95cf13b6492e58c521cde97aa50f23e3457dd431
Jin <bomjan@naver.com>
2020-01-22 00:46:59 +0000

    modified new client message

--- server.py
+++ server.py
@@ -30,12 +30,12 @@ def connectionLoop(sock):
             clients[addr] = {}
             clients[addr]['lastBeat'] = datetime.now()
             clients[addr]['color'] = 0
-            message = {"cmd": 0,"player":{"id":str(addr)}}
-            connectedClients = {"cmd": 2, "current_players": []}
+            message = {"cmd": 0,"player":[{"id":str(addr)}]}
+            connectedClients = {"cmd": 3, "player": []}
             m = json.dumps(message)
             for c in clients:
                sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
-               connectedClients['current_players'].append({"id":str(c)})
+               connectedClients['player'].append({"id":str(c)})
             m = json.dumps(connectedClients)
             sock.sendto(bytes(m,'utf8'), (addr[0],addr[1]))
 
